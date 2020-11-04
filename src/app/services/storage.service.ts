6b51aa37c55405735386987d616561fd13656a1c
gaminho <riche.antoine.pro@gmail.com>
2020-11-04 19:45:52 +0100

    add storage service and song update

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import { Observable, from } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+    public uploadSong(song: File): Observable<any> {
+      return from(new Promise<any>((resolve, reject) => {
+        firebase.storage().ref().child('songs').child(song.name).put(song)
+        .then(d => resolve(d.ref.fullPath))
+        .catch(e => reject(e))
+      }));
+    }
+}
